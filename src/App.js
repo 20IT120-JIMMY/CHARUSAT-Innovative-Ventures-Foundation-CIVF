52d804e2c7337d8314a8e7329424a70bfb23206d
JR0915 <jimmyprajapati33@gmail.com>
2022-10-18 00:59:04 +0530

    Adding Animations , extra pages , login/register etc more...

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      {/* {popup?<Login style={{ position: "absolute" }} />} */}
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          {/* <Route path="login" element={<Login />} /> */}
+        </Route>
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/data.js
@@ -0,0 +1,117 @@
+import img1 from "../../images/news.jpg";
+
+const data = {
+  cardData: [
+    {
+      id: 1,
+      img: img1,
+      type: "blog 1",
+      desc: "At CIVF, our six values represent a strong belief in the contribution we are making tow",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 2,
+      img: img1,
+      type: "blog 2",
+      desc: "At CIVF, ords a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery.",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 3,
+      img: img1,
+      type: "blog 3",
+      desc: "At CIVF, a better society. Theassion, commitment, enduring quality and impactful socio-economic delivery.",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 4,
+      img: img1,
+      type: "blog 1",
+      desc: "At CIVF, our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery.",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 5,
+      img: img1,
+      type: "blog 1",
+      desc: "At CIVF ,impactful socio-economic delivery.",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 6,
+      img: img1,
+      type: "blog 1",
+      desc: "At CIVF, our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery.",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 7,
+      img: img1,
+      type: "blog 1",
+      desc: "desc 1",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 8,
+      img: img1,
+      type: "blog 1",
+      desc: "desc 1",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 9,
+      img: img1,
+      type: "blog 1",
+      desc: "desc 1",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 10,
+      img: img1,
+      type: "blog 1",
+      desc: "desc 1",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 11,
+      img: img1,
+      type: "blog 1",
+      desc: "desc 1",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+    {
+      id: 12,
+      img: img1,
+      type: "blog 1",
+      desc: "desc 1",
+      subdesc:
+        "our six values represent a strong belief in the contribution we are making towards a better society. These values dictate our actions and are based on a framework of passion, commitment, enduring quality and impactful socio-economic delivery",
+      date: "17 May 2015",
+    },
+  ],
+};
+export default data;
